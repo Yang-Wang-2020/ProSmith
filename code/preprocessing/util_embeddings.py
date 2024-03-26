0fbf4d750edbc4f397da148d3240a01eeff7dc2e
Yang-Wang-2020 <69257748+Yang-Wang-2020@users.noreply.github.com>
2024-03-26 15:30:11 -0400

    Update util_embeddings.py
    removed tabs and replaced by spaces

--- code/preprocessing/util_embeddings.py
+++ code/preprocessing/util_embeddings.py
@@ -1,13 +1,12 @@
 import os
 from os.path import join
 
-
 def create_empty_path(path):
-	try:
-		os.mkdir(path)
-	except:
+    try:
+        os.mkdir(path)
+    except:
         pass
 
-	all_files = os.listdir(path)
-	for file in all_files:
-		os.remove(join(path, file))
+    all_files = os.listdir(path)
+    for file in all_files:
+        os.remove(join(path, file))
